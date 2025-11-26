3ed217baf262e95cbc6bd6dfb60266ec06db8368
DanutSpafiu <spafiu.spafiu@yahoo.com>
2025-11-25 21:02:31 -0800

    added form

--- frontend/src/pages/AdaugareElev.jsx
+++ frontend/src/pages/AdaugareElev.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import '../styles/AdaugareElev.css'
 
 const AdaugareElev = () => {
   return (
-    <div>AdaugareElev</div>
+    <>
+      <h2 className='titlu'>Adăugare elev</h2>
+      <div className='chenarElev'>
+        <form className='elev-form' onSubmit={(e) => e.preventDefault()}>
+          <div className='form-row'>
+            <label className='label'>Nume complet</label>
+            <input className='input' type='text' name='nume' placeholder='Ex: Ion Popescu' />
+          </div>
+
+          <div className='form-row'>
+            <label className='label'>Clasa</label>
+            <input className='input' type='text' name='clasa' placeholder='Ex: 8A' />
+          </div>
+          
+               
+          <div className='form-actions'>
+            <button className='submit-btn' type='submit'>Salvează (demo)</button>
+          </div>
+        </form>
+      </div>
+    </>
   )
 }
 
